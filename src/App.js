007a2e1ea3c2f35b9b0a3db62fe21838c503c0d2
micsoni <micsoni@gmail.com>
2020-03-06 11:58:00 +0100

    added input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from "react";
+import superagent from "superagent";
 
 class App extends React.Component {
+  onSubimit = async event => {
+    event.preventDefault();
+    try {
+      const response = await superagent
+        .post("http://localhost:4000/message")
+        .send({ text: "hardcoded" });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   render() {
-    return <div>"hello"</div>;
+    return (
+      <main>
+        <form onSubmit={this.onSubimit}>
+          <input type="text"></input>
+          <button>Send</button>
+        </form>
+      </main>
+    );
   }
 }
 
